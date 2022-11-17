18d35a1eac74c01237d2457fce93509a1416cdb5
eitangold <75008589+eitangold@users.noreply.github.com>
2022-11-17 21:55:02 +0200

    this is 2 commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("hello this is change 1")
+    print("this is second commit")
 
 
 # Press the green button in the gutter to run the script.
